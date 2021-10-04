a8bb17871cf94d8c1109961e7298db1c49806769
SERENA BADESHA <sbadesha@gmail.com>
2021-10-04 11:47:32 +0100

    Adding and subtracting tuples and tests

--- src/tuple.cpp
+++ src/tuple.cpp
@@ -1,20 +1,73 @@
 #include "tuple.hpp"
 
 bool isVector(tuple_t t) {
-    t.w == 0.0;
-    return true; 
+    return t.w == 0.0f;
 }
 
 bool isPoint(tuple_t t) {
-    t.w == 1.0;
-    return true; 
+    return t.w == 1.0f;
 }
 
 tuple_t newPoint(float x, float y, float z){
-    tuple_t TuplePoint{.x = x, .y = y, .z = z, .w = 1.0};
+    tuple_t TuplePoint{.x = x, .y = y, .z = z, .w = 1.0f};
     return TuplePoint;
 }
 tuple_t newVector(float x, float y, float z){
-    tuple_t TupleVector{.x = x, .y = y, .z = z, .w = 0};
+    tuple_t TupleVector{.x = x, .y = y, .z = z, .w = 0.0f};
     return TupleVector;
 }
+
+bool tuplesEqual(tuple_t t1, tuple_t t2) {
+    const float epsilon = 0.00001;
+    if (t1.x - t2.x < epsilon) {
+        return false; 
+    } 
+    if (t1.y - t2.y < epsilon) {
+        return false; 
+    } 
+    if (t1.z - t2.z < epsilon) {
+        return false; 
+    } 
+
+    return true; 
+}
+tuple_t addTuple(tuple_t t1, tuple_t t2) {
+    tuple_t t3{.x = 0, .y = 0, .z = 0, .w = 0.0f};
+    t1.x + t2.x = t3.x;
+    t1.y + t2.y = t3.y;
+    t1.z + t2.z = t3.z;
+    if (t1.w + t2.w != 2) {
+        t1.w + t2.w = t3.w; 
+    }
+    else {
+        t1.w = t3.w; 
+    }
+}
+
+tuple_t subtractTuple(tuple_t t1, tuple_t t2) {
+    tuple_t t3{.x = 0, .y = 0, .z = 0, .w = 0.0f};
+    t1.x - t2.x = t3.x;
+    t1.y + t2.y = t3.y;
+    t1.z + t2.z = t3.z;
+    if (t1.w + t2.w != 2) {
+        t1.w + t2.w = t3.w; 
+    }
+    else {
+        t1.w = t3.w; 
+    }
+return
+}
+
+tuple_t multiplyTuple(tuple_t t1, tuple_t t2) {
+    tuple_t t3{.x = 0, .y = 0, .z = 0, .w = 0.0f};
+    t1.x + t2.x = t3.x;
+    t1.y + t2.y = t3.y;
+    t1.z + t2.z = t3.z;
+    if (t1.w + t2.w != 2) {
+        t1.w + t2.w = t3.w; 
+    }
+    else {
+        t1.w = t3.w; 
+    }
+}
+
--- test/tuple_unit_test.cpp
+++ test/tuple_unit_test.cpp
@@ -3,7 +3,6 @@
 #include "gtest/gtest.h"
 namespace {
 
-
 // Tests isVector().
 
 // Test 
@@ -13,51 +12,27 @@ TEST(isVectorTest, true) {
  tuple_t testTupleVector;
  //tuple_t testTupleVector{.x = 4.3, .y = -4.2, .z = 3.1, .w = 0};
  //tuple_t testTuplePoint{.x = 4.3, .y = -4.2, .z = 3.1, .w = 1.0};
- testTuplePoint = newPoint(4.3, -4.2, 3.1); 
+
  testTupleVector = newVector(4.3, -4.2, 3.1);
-
  EXPECT_EQ(true, isVector(testTupleVector));
- EXPECT_EQ(true, isVector(testTuplePoint));
-//EXPECT_EQ(true, false);
 
-// // Tests factorial of 0.
-// TEST(FactorialTest, Zero) {
-//   EXPECT_EQ(1, Factorial(0));
+ testTuplePoint = newPoint(4.3, -4.2, 3.1); 
+ EXPECT_EQ(false, isVector(testTuplePoint));
 }
+ TEST(addTuple, true) {
 
-// // Tests factorial of positive numbers.
-// TEST(FactorialTest, Positive) {
-//   EXPECT_EQ(1, Factorial(1));
-//   EXPECT_EQ(2, Factorial(2));
-//   EXPECT_EQ(6, Factorial(3));
-//   EXPECT_EQ(40320, Factorial(8));
-// }
+ tuple_t testTuplePoint;
+ tuple_t testTupleVector; 
+
+ tuple 
+
+ testTupleVector = newVector(4.3, -4.2, 3.1);
+ testTuplePoint = newPoint(4.3, -4.2, 3.1); 
+ EXPECT_EQ(true, addTuple(testTupleVector, testTupleVector));
+
+ testTuplePoint = newPoint(4.3, -4.2, 3.1); 
+ EXPECT_EQ(false, addTuple(testTuplePoint, testTupleVector));
+ }
 
 
-// // Tests IsPrime()
-
-// // Tests negative input.
-// TEST(IsPrimeTest, Negative) {
-//   // This test belongs to the IsPrimeTest test case.
-
-//   EXPECT_FALSE(IsPrime(-1));
-//   EXPECT_FALSE(IsPrime(-2));
-//   EXPECT_FALSE(IsPrime(INT_MIN));
-// }
-
-// // Tests some trivial cases.
-// TEST(IsPrimeTest, Trivial) {
-//   EXPECT_FALSE(IsPrime(0));
-//   EXPECT_FALSE(IsPrime(1));
-//   EXPECT_TRUE(IsPrime(2));
-//   EXPECT_TRUE(IsPrime(3));
-// }
-
-// // Tests positive input.
-// TEST(IsPrimeTest, Positive) {
-//   EXPECT_FALSE(IsPrime(4));
-//   EXPECT_TRUE(IsPrime(5));
-//   EXPECT_FALSE(IsPrime(6));
-//   EXPECT_TRUE(IsPrime(23));
-// }
-}  // namespace
+} 
